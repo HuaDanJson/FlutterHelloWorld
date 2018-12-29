a45799d1a4937ec94fc56f1c9c60dd6dae0022eb
张海龙 <381266395@qq.com>
2018-12-29 21:13:56 +0800

    add  english_words: ^3.1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,6 +7,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final WordPair wordPair = new WordPair.random();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -13,10 +15,14 @@ class MyApp extends StatelessWidget {
       ),
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("Welcome to Flutter"),
+          title: new Text(
+            "Welcome to Flutter",
+            textAlign: TextAlign.center,
+          ),
         ),
         body: new Center(
-          child: new Text("Hello World"),
+//          child: new Text("Hello World"),
+          child: new Text(wordPair.asPascalCase),
         ),
       ),
     );
