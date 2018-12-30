1196594c706bbefc97f288f9b5c7b45e3583b15b
张海龙 <381266395@qq.com>
2018-12-30 13:17:30 +0800

    add RandomWords extends StatefulWidget and RandomWordsState extends State<RandomWords>

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final WordPair wordPair = new WordPair.random();
+//    final WordPair wordPair = new WordPair.random();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -22,9 +22,27 @@ class MyApp extends StatelessWidget {
         ),
         body: new Center(
 //          child: new Text("Hello World"),
-          child: new Text(wordPair.asPascalCase),
+//          child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final WordPair wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
